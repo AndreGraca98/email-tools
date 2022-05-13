a08d95bc9c9f8c08f467c913bedc9eed6e8a4c77
AndreGraca98 <andre.p.g@sapo.pt>
2022-05-13 14:32:36 +0100

    FIX last commit error

--- setup.py
+++ setup.py
@@ -12,9 +12,3 @@ setup(name='email_tools',
         'cryptography',
     ],
      )
-
-
-from email_tools import create_credentials_csv, generate_key
-
-generate_key()
-create_credentials_csv(force=True, MANUAL=True)
