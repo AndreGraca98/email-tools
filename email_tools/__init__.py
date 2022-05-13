aea3b49e01e246732b2238b80340dcbbfa8730e9
AndreGraca98 <andre.p.g@sapo.pt>
2022-05-13 13:58:20 +0100

    FIX import __init__ wrapper

--- email_tools/__init__.py
+++ email_tools/__init__.py
@@ -1,3 +1,3 @@
 from .encryption import get_data, get_credentials, get_all_credentials, get_pwd, get_email, generate_key, create_credentials_csv
 from .email_class import Email
-from .runner_wrapper import example_function, email_notification
+from .runner_wrapper import example_function, email_notification_wrapper
